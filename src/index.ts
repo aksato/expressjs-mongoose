b36d50262e36182b98761c0fbd3a8f892c285823
Andre Sato <andre.kubagawa@usp.br>
2023-04-24 09:49:05 -0300

    Form handling

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "./lib/db";
 import express from "express";
 import { NextFunction, Request, Response } from "express";
 import { engine } from "express-handlebars";
+import bodyParser from "body-parser";
 import * as handlers from "./lib/handlers";
 
 const app = express();
@@ -19,11 +20,17 @@ app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.render("home"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", handlers.home);
 
 app.get("/about", handlers.about);
+
+app.get("/newsletter-signup", handlers.newsletterSignup);
+
+app.post("/newsletter-signup/process", handlers.newsletterSignupProcess);
+
+app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou);
 
 // custom 404 page
 app.use(handlers.notFound);
--- src/lib/handlers.ts
+++ src/lib/handlers.ts
@@ -9,3 +9,20 @@ export const serverError = (
   res: Response,
   next: NextFunction
 ) => res.render("500");
+
+export const newsletterSignup = (req: Request, res: Response) => {
+  // we will learn about CSRF later...for now, we just
+  // provide a dummy value
+  res.render("newsletter-signup", { csrf: "CSRF token goes here" });
+};
+
+export const newsletterSignupProcess = (req: Request, res: Response) => {
+  console.log("Form (from querystring): " + req.query.form);
+  console.log("CSRF token (from hidden form field): " + req.body._csrf);
+  console.log("Name (from visible form field): " + req.body.name);
+  console.log("Email (from visible form field): " + req.body.email);
+  res.redirect(303, "/newsletter-signup/thank-you");
+};
+
+export const newsletterSignupThankYou = (req: Request, res: Response) =>
+  res.render("newsletter-signup-thank-you");
